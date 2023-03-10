16ec22988990bedabe20ba73675aa4b13ccccbaf
wilson <w54545254121@gmail.com>
2023-03-10 11:21:40 +0800

    second commit

---
+++ hello.py
@@ -0,0 +1,12 @@
+#write me a python helloworld?
+# Hello, World
+# ============
+
+# A simple program, and an example of how to structure a python project.
+# You can simply start the praogram by typing "python main.py" in the helloworld folder.
+
+
+#Source: https://github.com/dbarnett/python-helloworld
+
+print("Hello World")
+print("")
